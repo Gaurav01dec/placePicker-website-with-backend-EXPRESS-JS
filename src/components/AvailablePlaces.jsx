894e799302c9e4cbb338ccd9bd674ab10cfde97a
Gaurav01dec <tech.gauravverma@gmail.com>
2024-06-10 16:22:10 +0530

    put request work

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,69 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
+
+// const places = localStorage.getItem('places')
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  // TODO : FETCH AVAILABLE PLACES FROM THE BACKEND API FROM WHERE WE CAN FETCH OUR DATA 
+
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState()
+
+  // as we are setting the state in the fetch  last then block then the function will goes into the infinite loop so
+  // we have to use side effect hook here to tackle the infinite loop 
+
+  //METHOD 1 
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/places").then((response) => {
+  //     return response.json()
+  //   }).then((resData) => {
+  //     setAvailablePlaces(resData.places)
+  //   })
+  // }, [])
+
+  //METHOD 2 
+  // you cannot use the async await directly in react but you can create the extra functions by which this can happen 
+  useEffect(() => {
+    async function fetchPlaces() {
+
+      try {
+        const places = await fetchAvailablePlaces();
+        // now we are trying to set the locations based of the nearest of the user location
+        navigator.geolocation.getCurrentPosition((position)=>{
+
+          const sortedPlaces  = sortPlacesByDistance(places , position.coords.latitude, position.coords.longitude);
+
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+
+
+      } catch (error) {
+        setError({message: error.message || "Could not found Places, Please try again later"});
+        setIsFetching(false);
+      }
+
+      
+    };
+    fetchPlaces();
+  });
+
+  if(error){
+    return <Error title="An error Occured!" message={error.message}/>
+  }
+
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching some data ... please wait a while ... "
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
---
+++ src/http.js
@@ -0,0 +1,30 @@
+export async function fetchAvailablePlaces() {
+    const response = await fetch("http://localhost:3000/places");
+    const resData = await response.json();
+
+    // checking that server does't throw an error due to many issues
+    if (!response.ok) {
+        throw new Error("Failed to fetch places...")
+    }
+    return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+    // we have to update the fetch configuration to update the data , in get we don't need to do it 
+    // but we are using the put http verb soo we have to update it........................
+    const response = await fetch("http://localhost:3000/user-places", {
+        method: 'PUT',
+        //places is an array but we have to conert it to json format
+        body: JSON.stringify(places),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const resData = await response.json()
+
+    if (!response.ok) {
+        throw new Error('Failed to update the user data .....')
+    }
+    return resData.message
+}
